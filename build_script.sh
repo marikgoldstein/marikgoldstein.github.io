d63b286c0362f50d3fc8246c260c3dc284aadab5
Mark <goldstein.marik@gmail.com>
2018-06-20 12:41:20 -0400

    .

--- build_script.sh
+++ build_script.sh
@@ -1,5 +1,5 @@
 rm -rf public
 git worktree prune
-git worktree add -B master public/ master
+git worktree add -b master public/ origin/master
 hugo
 cd public && git add --all && git commit -m "." && cd ..
