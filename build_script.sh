07916a449a61b612bbc3b9fd2f6a4f604243212e
Mark <goldstein.marik@gmail.com>
2018-06-20 12:26:42 -0400

    .

--- build_script.sh
+++ build_script.sh
@@ -2,5 +2,4 @@ rm -rf public
 git worktree prune
 git worktree add -B master public/ upstream/master
 hugo
-#echo "rachitsingh.com" > public/CNAME
 cd public && git add --all && git commit -m "." && cd ..
